78733155c13db9e9b2b00623211112dc430899d4
ICHINOMIYA1038 <ryohei108@icloud.com>
2023-06-19 16:42:30 +0900

    first

---
+++ app/pages/index.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+const Home: React.FC<HomeProps> = (props) => {
+  return (
+    <div>
+      <h2>POSTの一覧</h2>
+      <table>
+      <tbody>
+        {props.posts.map((post) => (
+          <tr key={post.id}>
+            <td>{post.id}.</td>
+            <td>{post.title}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  try {
+    const response = await fetch('http://api:3000/posts', { method: 'GET' });
+    const json = await response.json();
+
+    return {
+      props: {
+        posts: json,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    return {
+      props: {
+        posts: [],
+      },
+    };
+  }
+};
+
+export default Home;
