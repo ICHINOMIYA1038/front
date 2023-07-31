ba278816e31bec2d29739b4df6e7a547567640d5
ICHINOMIYA <ichiryo108@gmail.com>
2023-07-31 23:01:21 +0900

    aa

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
 import Layout from '@/components/Layout';
 import SearchForm from '@/components/search';
 import PostCard from '@/components/PostCard';
 import { useRouter } from 'next/router';
-import { Pagination } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Pagination, CircularProgress } from '@mui/material';
 
 interface Post {
   id: number;
@@ -21,14 +21,9 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = (props: any) => {
   const router = useRouter();
-  const [page, setPage] = useState(1);
-  const [per, setPer] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [pagination, setPagination] = useState<any>({});
 
   const handlePageChange = (event: any, newPage: number) => {
-    setPage(newPage);
     const searchParams = { ...props.query };
     searchParams.page = newPage;
     searchParams.per = props.pagination.limit_value;
@@ -36,71 +31,34 @@ const Home: React.FC<HomeProps> = (props: any) => {
     router.push(`/?${queryString}`);
   };
 
+  useEffect(() => {
+    setLoading(false); // 仮に、この例では即座にデータが取得されたものとする
+  }, []);
+
   return (
     <Layout>
       <SearchForm />
       {loading ? (
-        <div>Loading...</div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
+          <CircularProgress />
+        </div>
       ) : (
         <>
-          {posts.map((post: Post) => (
+          {props.posts.map((post: Post) => (
             <PostCard key={post.id} post={post} />
           ))}
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
             <Pagination
-              count={pagination.total_pages} // 総ページ数
+              count={props.pagination.total_pages} // 総ページ数
               color="primary" // ページネーションの色
               onChange={handlePageChange}
-              page={pagination.current_page} // 現在のページ番号
+              page={props.pagination.current_page} // 現在のページ番号
             />
           </div>
         </>
       )}
     </Layout>
   );
-};
-
-export const getStaticProps = async ({ query }: any) => {
-  try {
-    const page = query.page || 1;
-    const per = query.per || 8;
-    const queryString = new URLSearchParams(query).toString();
-
-    // APIリクエストのタイムアウト設定
-    const fetchTimeout = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Request timed out')), 4000)
-    );
-
-    // 実際のAPIリクエスト
-    const apiRequest = fetch(`${process.env.NEXT_PUBLIC_RAILS_API}/search`, { method: 'GET' })
-      .then((response) => response.json())
-      .catch((error) => {
-        console.error('Error fetching posts:', error);
-        return {
-          posts: [],
-          pagination: { total_pages: 0, current_page: 0, limit_value: 0 },
-        };
-      });
-
-    // タイムアウトとAPIリクエストの両方の結果を競争させる
-    const response = await Promise.race([fetchTimeout, apiRequest]);
-
-    return {
-      props: {
-        posts: response.posts,
-        pagination: response.pagination,
-        query: query,
-      },
-    };
-  } catch (error) {
-    console.error('Error handling server-side props:', error);
-    return {
-      props: {
-        posts: [],
-        pagination: { total_pages: 0, current_page: 0, limit_value: 0 },
-      },
-    };
-  }
 };
 
 export default Home;
