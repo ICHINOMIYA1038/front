bc00e0bb975ba5aef71e2e49170166f252666c51
ICHINOMIYA <ichiryo108@gmail.com>
2023-08-01 00:37:48 +0900

    timeOutError Fixed

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -57,7 +57,7 @@ export const getServerSideProps = async ({query}:any) => {
     const page = query.page || 1;
     const per = query.per || 8;
     const queryString = new URLSearchParams(query).toString();
-    const response = await fetch(`${process.env.NEXT_PUBLIC_RAILS_API}/search?${queryString}&paged=${page}&per=${per}`, { method: 'GET' });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_RAILS_API}/search`, { method: 'GET' });
     const json = await response.json();
     
     return {
