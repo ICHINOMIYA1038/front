38df6630a2651c0a1c87181cb192f540b5051cc1
ICHINOMIYA <ichiryo108@gmail.com>
2023-07-31 22:16:14 +0900

    fix timeout error

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,8 +1,8 @@
+import React, { useState, useEffect } from 'react';
 import Layout from '@/components/Layout';
 import SearchForm from '@/components/search';
 import PostCard from '@/components/PostCard';
 import { useRouter } from 'next/router';
-import {useState} from 'react'
 import { Pagination } from '@mui/material';
 
 interface Post {
@@ -12,59 +12,92 @@ interface Post {
 
 interface HomeProps {
   posts: Post[];
+  pagination: {
+    total_pages: number;
+    current_page: number;
+    limit_value: number;
+  };
 }
 
-
-//Homeコンポーネント
-const Home: React.FC<HomeProps> = (props:any) => {
+const Home: React.FC<HomeProps> = (props: any) => {
   const router = useRouter();
-  const [page, setPage] = useState(1)
-  const [per, setPer] = useState(1)
+  const [page, setPage] = useState(1);
+  const [per, setPer] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [pagination, setPagination] = useState<any>({});
 
-  const handlePageChange = (event:any, newPage:any) => {
+  const handlePageChange = (event: any, newPage: number) => {
     setPage(newPage);
-    const searchParams = props.query ; // 既存のクエリパラメータをコピー
-  
-    searchParams.page = newPage; // ページを更新
-    searchParams.per = props.pagination.limit_value; // 1ページあたりの件数を維持（もしくは新しい値に更新）
-  
+    const searchParams = { ...props.query };
+    searchParams.page = newPage;
+    searchParams.per = props.pagination.limit_value;
     const queryString = new URLSearchParams(searchParams).toString();
     router.push(`/?${queryString}`);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_RAILS_API}/search?paged=${page}&per=${per}`,
+          { method: 'GET' }
+        );
+        const json = await response.json();
+
+        setPosts(json.posts);
+        setPagination(json.pagination);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+        setPosts([]);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [page, per]);
+
   return (
     <Layout>
-      <SearchForm/>
-      {props.posts.map((post: { post_id: any; }) => (
-   < PostCard key={post.post_id} post={post} />
-    ))}
-
-  <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-      
-      <Pagination 
-        count={props.pagination.total_pages}          //総ページ数
-        color="primary"     //ページネーションの色
-        onChange={handlePageChange}
-        page={props.pagination.current_page}         //現在のページ番号
-        />
-    </div>
+      <SearchForm />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {posts.map((post: Post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <Pagination
+              count={pagination.total_pages} // 総ページ数
+              color="primary" // ページネーションの色
+              onChange={handlePageChange}
+              page={pagination.current_page} // 現在のページ番号
+            />
+          </div>
+        </>
+      )}
     </Layout>
   );
 };
 
-export const getServerSideProps = async ({query}:any) => {
+export const getServerSideProps = async ({ query }: any) => {
   try {
     const page = query.page || 1;
     const per = query.per || 8;
     const queryString = new URLSearchParams(query).toString();
-    const response = await fetch(`${process.env.NEXT_PUBLIC_RAILS_API}/search?${queryString}&paged=${page}&per=${per}`, { method: 'GET' });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_RAILS_API}/search?${queryString}&paged=${page}&per=${per}`,
+      { method: 'GET' }
+    );
     const json = await response.json();
-    
+
     return {
       props: {
         posts: json.posts,
         pagination: json.pagination,
-        query:query
+        query: query,
       },
     };
   } catch (error) {
@@ -72,6 +105,7 @@ export const getServerSideProps = async ({query}:any) => {
     return {
       props: {
         posts: [],
+        pagination: { total_pages: 0, current_page: 0, limit_value: 0 },
       },
     };
   }
