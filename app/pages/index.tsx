abf5abc92248d0fa07e03cfc71a6012313cb3d58
ICHINOMIYA <ichiryo108@gmail.com>
2023-09-18 20:18:38 +0900

    ok

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -47,38 +47,35 @@ const getPosts = async ({ queryKey }: any): Promise<any> => {
   return response.json();
 };
 
-
-
 //Homeコンポーネント
 const Home: React.FC<HomeProps> = (props: any) => {
   const queryString = new URLSearchParams(props.query).toString();
 
   const {
-    data:newsdata,
+    data: newsdata,
     isLoading,
     // isFetching,
-    error
+    error,
   } = useQuery({
-  queryKey: ["news"],
-  queryFn: getNewsData,
-  refetchOnWindowFocus: false,
-  staleTime: 1000 * 60 * 5,
-  cacheTime: Infinity,
+    queryKey: ["news"],
+    queryFn: getNewsData,
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 5,
+    cacheTime: Infinity,
   });
 
   const {
-    data:postdata,
-    isLoading:isLoadingPost,
+    data: postdata,
+    isLoading: isLoadingPost,
     // isFetching,
-    error:errorPost
-} = useQuery({
-  queryKey: [queryString],
-  queryFn: getPosts,
-  refetchOnWindowFocus: false,
-  staleTime: 100 * 60 * 5,
-  cacheTime: Infinity,
+    error: errorPost,
+  } = useQuery({
+    queryKey: [queryString],
+    queryFn: getPosts,
+    refetchOnWindowFocus: false,
+    staleTime: 100 * 60 * 5,
+    cacheTime: Infinity,
   });
-
 
   const router = useRouter();
   const [sort_by, setSortBy] = useState(0);
@@ -86,11 +83,11 @@ const Home: React.FC<HomeProps> = (props: any) => {
 
   const handlePageChange = (event: any, newPage: any) => {
     const searchParams = props.query; // 既存のクエリパラメータをコピー
-    
+
     searchParams.page = newPage; // ページを更新
     searchParams.per = postdata.pagination.limit_value; // 1ページあたりの件数を維持（もしくは新しい値に更新）
     const queryString = new URLSearchParams(searchParams).toString();
-   
+
     router.push(`/?${queryString}`);
   };
 
@@ -112,42 +109,41 @@ const Home: React.FC<HomeProps> = (props: any) => {
             onSortChange={handleSortChange}
           />
         </div>
-        <div className="mx-10 mt-5">
+        <div className="mx-10 mt-5 lg:w-1/2">
           <div className="hidden lg:block">
-          <SortComponent
-            sort_by={sort_by}
-            sortDirection={sortDirection}
-            onSortChange={handleSortChange}
-          />
+            <SortComponent
+              sort_by={sort_by}
+              sortDirection={sortDirection}
+              onSortChange={handleSortChange}
+            />
           </div>
-          {postdata && postdata.posts.map((post: { post_id: any }) => (
-            <PostCard key={post.post_id} post={post} />
-          ))}
+          {postdata &&
+            postdata.posts.map((post: { post_id: any }) => (
+              <PostCard key={post.post_id} post={post} />
+            ))}
         </div>
       </div>
 
-
       <div className="justify-center my-10 flex">
-        {postdata &&
-        <Pagination
-          count={postdata.pagination.total_pages} //総ページ数
-          color="primary" //ページネーションの色
-          onChange={handlePageChange}
-          page={postdata.pagination.current_page} //現在のページ番号
-        />
-}
+        {postdata && (
+          <Pagination
+            count={postdata.pagination.total_pages} //総ページ数
+            color="primary" //ページネーションの色
+            onChange={handlePageChange}
+            page={postdata.pagination.current_page} //現在のページ番号
+          />
+        )}
       </div>
     </Layout>
   );
 };
 
 export const getServerSideProps = async ({ query }: any) => {
-  
-    return {
-      props: {
-        query: query || [],
-      },
-    };
+  return {
+    props: {
+      query: query || [],
+    },
+  };
 };
 
 export default Home;
